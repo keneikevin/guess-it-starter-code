ef3fdbcc599070c0b1fe09919a3b3187964fd4b0
keneikevin <kevinkenei9@gmail.com>
2020-01-11 11:45:19 +0300

    creating GameViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,11 +17,14 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -30,7 +33,7 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
-
+    private lateinit var viewModel: GameViewModel
     // The current word
     private var word = ""
 
@@ -46,13 +49,10 @@ class GameFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
 
         // Inflate view and obtain an instance of the binding class
-        binding = DataBindingUtil.inflate(
-                inflater,
-                R.layout.game_fragment,
-                container,
-                false
+        binding = DataBindingUtil.inflate(inflater, R.layout.game_fragment, container, false
         )
-
+        Log.i("GameFragment","Called ViewModelProviders.of! ")
+        viewModel =ViewModelProviders.of(this).get(GameViewModel::class.java)
         resetList()
         nextWord()
 
